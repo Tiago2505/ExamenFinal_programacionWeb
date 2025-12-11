a733b9e9997e4e72b591d471d6de9af25c65c322
Dany Paz <danysantiago2505@gmail.com>
2025-12-10 21:12:57 -0600

    aniadiendo controlador libro

---
+++ examen_final/Controllers/LibroController.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using examen_final.DTOs;
+using examen_final.Services;
+
+namespace examen_final.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LibroController : ControllerBase
+    {
+        
+        
+    }
+}
